f5cc85cf6d6ed062dbf45504285aaf11303c8bab
Yuriy Dubinin <yuradubinin100@gmail.com>
2023-12-15 14:09:33 +0300

    Init

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+async function start() {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        app.listen(PORT, () => {
+            console.log(`Server successfully started on PORT: ${PORT}`);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
